c5b604e727bedf14ca5cf67684fdb63fe49a26ce
Aleksandar Perisic <fizikaneri@gmail.com>
2023-06-05 15:58:33 +0200

    try JS Redirect and BUILD 1.0.29

--- source/script.js
+++ source/script.js
@@ -491,7 +491,7 @@ document.addEventListener('DOMContentLoaded', function() {
         if (window.location.href === "https://support.regiondo.com/de");
         {
             // window.location.href = "https://support.regiondo.com/hc/de-de/";
-            location.href = "https://support.regiondo.com/hc/de-de/";
+            // location.href = "https://support.regiondo.com/hc/de-de/";
             // console.log('p', window.location.pathname  );
             // console.log('h', window.location.href );
             // console.log( window.location.href === "https://support.regiondo.com/de" );
