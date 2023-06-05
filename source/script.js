84fbae11198f385c032e92c16594a3b84d6e8ca0
Aleksandar Perisic <fizikaneri@gmail.com>
2023-06-05 16:07:29 +0200

    try JS Redirect and BUILD 1.0.33

--- source/script.js
+++ source/script.js
@@ -483,23 +483,25 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
 
-    /// redirects
-    // https://support.regiondo.com/de
-    // https://support.regiondo.com/hc/de-de
-    // var isErrorPage = $(".error-page").length > 0;
-    const isErrorPage = document.getElementsByClassName('error-page')[0];
 
-    window.onload = function() {
-        if (isErrorPage && window.location.href === "https://support.regiondo.com/de");
-        {
-            //window.location.href = "https://support.regiondo.com/hc/de-de/";
-            // location.href = "https://support.regiondo.com/hc/de-de/";
-            console.log('p', window.location.pathname  );
-            console.log('h', window.location.href );
-            console.log( window.location.href === "https://support.regiondo.com/de" );
-        }
-
-    }
 
 
 });
+
+
+/// redirects
+// https://support.regiondo.com/de
+// https://support.regiondo.com/hc/de-de
+// var isErrorPage = $(".error-page").length > 0;
+const isErrorPage = document.getElementsByClassName('error-page')[0];
+
+window.onload = function() {
+    if (isErrorPage && window.location.href === "https://support.regiondo.com/de");
+    {
+        window.location.href = "https://support.regiondo.com/hc/de-de/";
+        console.log('p', window.location.pathname  );
+        console.log('h', window.location.href );
+        console.log( window.location.href === "https://support.regiondo.com/de" );
+    }
+
+}
